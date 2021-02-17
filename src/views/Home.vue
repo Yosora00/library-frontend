6d3ff106233662714ed606467cf108f60fbcc27a
Yosora <1354464666@qq.com>
2021-02-17 10:14:14 +0800

    add home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,102 @@
 <template>
   <div>
-    <h1>首页</h1>
+    <!-- <img
+      src="http://image.nmc.cn/assets/img/w/40x40/4/1.png"
+      class="bg-secondary"
+      alt=""
+    /> -->
+    <!-- <div style="margin-top: 40px"> -->
+    <div class="mt-5">
+      <b-card-group deck>
+        <b-card border-variant="primary" align="center">
+          <b-card-text>
+            <h3>天气</h3>
+            <div>
+              <img
+                src="http://image.nmc.cn/assets/img/w/40x40/4/1.png"
+                style="background-color: #3783cf"
+                alt=""
+              />
+            </div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit.</b-card-text
+          >
+        </b-card>
+
+        <b-card border-variant="primary" align="center">
+          <b-card-text>
+            <h3>天气</h3>
+            <div>
+              <img
+                src="http://image.nmc.cn/assets/img/w/40x40/4/1.png"
+                style="background-color: #3783cf"
+                alt=""
+              />
+            </div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit.</b-card-text
+          >
+        </b-card>
+
+        <b-card border-variant="primary" align="center">
+          <b-card-text>
+            <h3>天气</h3>
+            <div>
+              <img
+                src="http://image.nmc.cn/assets/img/w/40x40/4/1.png"
+                style="background-color: #3783cf"
+                alt=""
+              />
+            </div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit.</b-card-text
+          >
+        </b-card>
+      </b-card-group>
+    </div>
+
+    <div class="mt-2">
+      <b-card-group deck>
+        <b-card border-variant="primary" align="center">
+          <b-card-text>
+            <h3>天气</h3>
+            <div>
+              <img
+                src="http://image.nmc.cn/assets/img/w/40x40/4/1.png"
+                class="bg-secondary"
+                alt=""
+              />
+            </div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit.</b-card-text
+          >
+        </b-card>
+
+        <b-card border-variant="primary" align="center">
+          <b-card-text>
+            <h3>天气</h3>
+            <h3>天气</h3>
+            <h3>天气</h3>
+            <h3>天气</h3>
+            <div>
+              <img
+                src="http://image.nmc.cn/assets/img/w/40x40/4/1.png"
+                class="bg-secondary"
+                alt=""
+              />
+            </div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit.</b-card-text
+          >
+        </b-card>
+      </b-card-group>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
-  data() {},
   methods: {},
 };
 </script>
